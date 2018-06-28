8b455115b2d685a1e881a5e7bc94e1cf69fc1654
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-06-28 03:05:45 +0000

    6/21の修正

--- 0621/062101.sh
+++ 0621/062101.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "1990年元旦からの曜日集計"
-for day in $(cat gantan)
+for day in $(cat ~/kadai/unix.2018/quiz.data/gantan)
 do
 	      echo $(date -d $day '+%u %a')
-done | sort -n | uniq -c | awk '{print $3,$1"回"}
+done | sort -n | uniq -c | awk '{print $3,$1"回"}'
